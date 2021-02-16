6982cfb7a4aca0f9998a1f73fcfd106e8e306193
Ananya Nigotia <nigotiaananya@gmail.com>
2021-02-17 02:31:35 +0530

    Navbar and footer done

--- visa_germany_app/views.py
+++ visa_germany_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
 
 # Create your views here.
+class Homepage(TemplateView):
+    template_name= "index.html"
+
+class Aboutpage(TemplateView):
+    template_name= "aboutus.html"
+
+class Contactpage(TemplateView):
+    template_name= "contactus.html"
--- visa_germany_project/settings.py
+++ visa_germany_project/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'visa_germany_app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'visa_germany_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+import os.path
 STATIC_URL = '/static/'
+STATIC_FILES_DIRS = (os.path.join('static'),)
